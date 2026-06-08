bc12c66918b341e4992137d1e53a2acc583b8a2d
vidyakeerthichitturi <vidyakeerthi.chitturi@klh.edu.in>
2026-06-08 21:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,20 @@ def merge_sort(arr):
 
     return merge(left, right)
 
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
 
